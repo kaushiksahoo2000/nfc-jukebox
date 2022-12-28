a97af9970b521e755a2def3c9c8a0f0f7959f8f8
Kaushik Sahoo <kaushiksahoo@Kaushiks-MacBook-Pro.local>
2022-12-28 15:43:09 -0800

    feat(nfc) get uids to show up

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import { NFC } from 'nfc-pcsc'
+import pretty from './pretty-logger.js'
 import { GetCurrentlyPlaying, GetRecentlyPlayed, GetTopTracks, GetDevices, PausePlayer, PlayPlayer } from './spotify.js'
 
+const nfc = new NFC()
 const app = express()
 const port = 4000
 
@@ -82,4 +85,80 @@ app.get('/', async (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+nfc.on('reader', async (reader) => {
+  pretty.info(`device attached`, reader)
+
+  // enable when you want to auto-process ISO 14443-4 tags (standard=TAG_ISO_14443_4)
+  // when an ISO 14443-4 is detected, SELECT FILE command with the AID is issued
+  // the response is available as card.data in the card event
+  // you can set reader.aid to:
+  // 1. a HEX string (which will be parsed automatically to Buffer)
+  reader.aid = 'F222222222'
+  // 2. an instance of Buffer containing the AID bytes
+  // reader.aid = Buffer.from('F222222222', 'hex');
+  // 3. a function which must return an instance of a Buffer when invoked with card object (containing standard and atr)
+  //    the function may generate AIDs dynamically based on the detected card
+  // reader.aid = ({ standard, atr }) => {
+  //
+  // 	return Buffer.from('F222222222', 'hex');
+  //
+  // };
+
+  reader.on('card', async (card) => {
+    pretty.info(`card detected`, reader, card)
+
+    // // example reading 4 bytes assuming containing 16bit integer
+    // // !!! note that we don't need 4 bytes - 16bit integer takes just 2 bytes !!!
+    // try {
+    //   // reader.read(blockNumber, length, blockSize = 4, packetSize = 16)
+    //   // - blockNumber - memory block number where to start reading
+    //   // - length - how many bytes to read
+    //   // - blockSize - 4 for MIFARE Ultralight, 16 for MIFARE Classic
+    //   // ! Caution! length must be divisible by blockSize (we have to read the whole block(s))
+
+    //   const data = await reader.read(4, 4)
+
+    //   pretty.info(`data read`, reader, data)
+
+    //   const payload = data.readInt16BE(0)
+
+    //   pretty.info(`data converted`, reader, payload)
+    // } catch (err) {
+    //   pretty.error(`error when reading data`, reader, err)
+    // }
+
+    // // example write 4 bytes containing 16bit integer
+    // // !!! note that we don't need 16 bytes - 16bit integer takes just 2 bytes !!!
+    // try {
+    //   // reader.write(blockNumber, data, blockSize = 4, packetSize = 16)
+    //   // - blockNumber - memory block number where to start writing
+    //   // - data - what to write
+    //   // - blockSize - 4 for MIFARE Ultralight, 16 for MIFARE Classic
+    //   // ! Caution! data.length must be divisible by blockSize (we have to write the whole block(s))
+
+    //   const data = Buffer.allocUnsafe(4).fill(0)
+    //   const randomNumber = Math.round(Math.random() * 1000)
+    //   data.writeInt16BE(randomNumber, 0)
+
+    //   await reader.write(4, data)
+
+    //   pretty.info(`data written`, reader, randomNumber, data)
+    // } catch (err) {
+    //   pretty.error(`error when writing data`, reader, err)
+    // }
+  })
+
+  reader.on('error', (err) => {
+    pretty.error(`an error occurred`, reader, err)
+  })
+
+  reader.on('end', () => {
+    pretty.info(`device removed`, reader)
+  })
+})
+
+nfc.on('error', (err) => {
+  pretty.error(`an error occurred`, err)
 })
---
+++ pretty-logger.js
@@ -0,0 +1,68 @@
+'use strict'
+
+// pretty-logger for debugging
+// uses great winston logger library, visit https://github.com/winstonjs/winston
+
+import util from 'util'
+import chalk from 'chalk'
+import winston from 'winston'
+import { SPLAT } from 'triple-beam'
+
+const colors = {
+  exception: 'red',
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  verbose: 'blue',
+  debug: 'blue',
+  silly: 'gray',
+}
+
+winston.addColors(colors)
+
+// we could use instanceof but to avoid import we simply check obj structure
+const isReader = (obj) => typeof obj === 'object' && obj.reader && obj.name
+
+const printf = winston.format.printf(({ timestamp, level, message, [SPLAT]: splat }) => {
+  let splatString = ''
+
+  let reader = ''
+
+  if (splat) {
+    let readerObj = splat.find(isReader)
+
+    if (readerObj) {
+      reader = chalk.cyan(readerObj.name) + ' '
+      splat = splat.filter((obj) => !isReader(obj))
+    }
+
+    if (splat.length > 1) {
+      splatString = ' ' + util.inspect(splat, { colors: true })
+    } else if (splat.length > 0) {
+      splatString = ' ' + util.inspect(splat[0], { colors: true })
+    }
+  }
+
+  // see https://stackoverflow.com/questions/10729276/how-can-i-get-the-full-object-in-node-jss-console-log-rather-than-object
+  return `${timestamp ? timestamp + ' – ' : ''}${reader}${level}: ${message}${splatString}`
+})
+
+const FORMAT = winston.format.combine(
+  winston.format.timestamp({
+    format: () => chalk.gray(new Date().toLocaleTimeString()),
+  }),
+  winston.format.colorize(),
+  printf,
+)
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      level: 'silly',
+      format: FORMAT,
+    }),
+  ],
+  exitOnError: true,
+})
+
+export default logger
